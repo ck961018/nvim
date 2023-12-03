7f3c13371667dab0e9729c0e95a0e3ebfb1de098
ck961018 <821632025@qq.com>
2023-12-03 22:17:13 +0800

    23/12/03

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -2,6 +2,7 @@ return {
     "hrsh7th/nvim-cmp",
     event = { "BufReadPost", "BufNewFile" },
     dependencies = {
+        "neovim/nvim-lspconfig",
         "hrsh7th/cmp-path",
         "hrsh7th/cmp-nvim-lsp",
         "hrsh7th/cmp-buffer",
