a30c898668038a85be8ae8913cc50209c5b8f6ea
ck961018 <821632025@qq.com>
2023-12-05 18:25:45 +0800

    23/12/05

--- lua/plugins/utils.lua
+++ lua/plugins/utils.lua
@@ -256,6 +256,18 @@ return {
             vim.cmd("autocmd! TermOpen term://* lua set_terminal_keymaps()")
         end,
     },
+    {
+        "vidocqh/auto-indent.nvim",
+        config = function()
+            require("auto-indent").setup({
+                lightmode = true, -- Lightmode assumes tabstop and indentexpr not change within buffer's lifetime
+                indentexpr = function(lnum)
+                    return require("nvim-treesitter.indent").get_indent(lnum)
+                end,
+                ignore_filetype = {}, -- Disable plugin for specific filetypes, e.g. ignore_filetype = { 'javascript' }
+            })
+        end
+    },
     -- 编码检测
     -- {
     --     "mbbill/fencview",
