ac1c83ea16fa9a60cf2f9e5df0c0f6380b61a809
ck961018 <ck961018@gmail.com>
2023-12-12 23:00:07 +0800

    23/12/12

--- lua/core/autocmds.lua
+++ lua/core/autocmds.lua
@@ -28,7 +28,7 @@ end
 
 vim.api.nvim_create_autocmd({ "VimEnter" }, {
     callback = function()
-        if vim.fn.has("win32") then
+        if vim.fn.has("win32") == 1 then
             local pid, nvim_num = read_hotkey_info()
             if pid == 0 then
                 local autohotkey_path = vim.fn.stdpath("config") .. [[/dependencies/bin/Win64/AutoHotkey_2.0.10]]
@@ -43,7 +43,7 @@ vim.api.nvim_create_autocmd({ "VimEnter" }, {
 
 vim.api.nvim_create_autocmd({ "ExitPre" }, {
     callback = function()
-        if vim.fn.has("win32") then
+        if vim.fn.has("win32") == 1 then
             local pid, nvim_num = read_hotkey_info()
             if nvim_num == 1 and pid ~= 0 then
                 vim.fn.jobstop(pid)
