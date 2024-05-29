a95256164c7ed650be72f41db1a13ccd8ae3da43
ck961018 <ck961018@git.com>
2024-05-29 09:58:26 +0800

    24/05/29

--- lua/plugins/edit.lua
+++ lua/plugins/edit.lua
@@ -4,8 +4,8 @@ return {
         config = function()
             require("im_select").setup({
                 default_command = vim.uv.os_uname().sysname == "Windows_NT"
-                        and vim.fn.stdpath("config") .. "/dependencies/env/win64/bin/ds-pinyin-lsp.exe"
-                    or "/dependencies/env/wsl/bin/ds-pinyin-lsp.exe",
+                        and vim.fn.stdpath("config") .. "/dependencies/env/win64/bin/im-select.exe"
+                    or vim.fn.stdpath("config") .. "/dependencies/env/wsl/bin/im-select.exe",
             })
         end,
     },
