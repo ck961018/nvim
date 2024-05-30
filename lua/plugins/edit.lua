ef1cd85cc2f29c343c2c6172fb62d18eaf340748
ck961018 <ck961018@gmail.com>
2024-05-30 22:07:03 +0800

    24/05/30

--- lua/plugins/edit.lua
+++ lua/plugins/edit.lua
@@ -2,13 +2,7 @@ return {
     {
         "keaising/im-select.nvim",
         event = "LazyFile",
-        config = function()
-            require("im_select").setup({
-                default_command = vim.uv.os_uname().sysname == "Windows_NT"
-                        and vim.fn.stdpath("config") .. "/dependencies/env/win64/bin/im-select.exe"
-                    or vim.fn.stdpath("config") .. "/dependencies/env/wsl/bin/im-select.exe",
-            })
-        end,
+        opts = {},
     },
     {
         "vidocqh/auto-indent.nvim",
---
+++ lua/test.lua
@@ -0,0 +1,2 @@
+local a = vim.fn.stdpath("config") .. "/dependencies/env/wsl/bin/im-select.exe"
+vim.print(a)
