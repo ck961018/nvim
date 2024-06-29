4263e1aeaff34b590393b4566ac430c7cafe2c84
成可 <chengke@fscut.com>
2024-06-29 18:10:48 +0800

    24/06/29

--- lua/plugins/cmake.lua
+++ lua/plugins/cmake.lua
@@ -3,55 +3,20 @@ return {
         "Civitasv/cmake-tools.nvim",
         keys = {
             -- { "<F5>", mode = { "n", "v" }, [[<cmd>CMakeRun<CR>]] },
-            { "<F6>", mode = { "n", "v" }, [[<cmd>CMakeSelectBuildPreset<CR>]] },
+            { "<F6>", mode = { "n", "v" }, [[<cmd>CMakeSelectCwd<CR>]] },
             { "<F7>", mode = { "n", "v" }, [[<cmd>CMakeBuild<CR>]] },
 
             -- { "<F5>", mode = "i", [[<ESC><cmd>CMakeRun<CR>]] },
-            { "<F6>", mode = "i", [[<ESC><cmd>CMakeSelectBuildPreset<CR>]] },
+            { "<F6>", mode = "i", [[<ESC><cmd>CMakeSelectCwd<CR>]] },
             { "<F7>", mode = "i", [[<ESC><cmd>CMakeBuild<CR>]] },
         },
         opts = {
-            cmake_command = "cmake", -- this is used to specify cmake command path
-            cmake_regenerate_on_save = true, -- auto generate when save CMakeLists.txt
-            -- cmake_generate_options = { "-DCMAKE_EXPORT_COMPILE_COMMANDS=1" }, -- this will be passed when invoke `CMakeGenerate`
-            cmake_build_options = {}, -- this will be passed when invoke `CMakeBuild`
-            -- support macro expansion:
-            --       ${kit}
-            --       ${kitGenerator}
-            --       ${variant:xx}
             cmake_build_directory = "build", -- this is used to specify generate directory for cmake, allows macro expansion
             cmake_soft_link_compile_commands = false, -- this will automatically make a soft link from compile commands file to project root dir
             cmake_compile_commands_from_lsp = true, -- this will automatically set compile commands file location using lsp, to use it, please set `cmake_soft_link_compile_commands` to false
-            cmake_kits_path = nil, -- this is used to specify global cmake kits path, see CMakeKits for detailed usage
-            cmake_variants_message = {
-                short = { show = true }, -- whether to show short message
-                long = { show = true, max_length = 40 }, -- whether to show long message
-            },
-            -- cmake_dap_configuration = {               -- debug settings for cmake
-            --     name = "cpp",
-            --     type = "codelldb",
-            --     request = "launch",
-            --     stopOnEntry = false,
-            --     runInTerminal = true,
-            --     console = "integratedTerminal",
-            -- },
             cmake_executor = { -- executor to use
                 name = "quickfix", -- name of the executor
-                opts = {}, -- the options the executor will get, possible values depend on the executor type. See `default_opts` for possible values.
                 default_opts = { -- a list of default and possible values for executors
-                    quickfix = {
-                        show = "always", -- "always", "only_on_error"
-                        position = "belowright", -- "bottom", "top"
-                        size = 10,
-                        encoding = "cp936",
-                        auto_close_when_success = false,
-                    },
-                    terminal = {}, -- terminal executor uses the values in cmake_terminal
-                },
-            },
-            cmake_runner = {
-                name = "quickfix",
-                default_opts = { -- a list of default and possible values for runners
                     quickfix = {
                         encoding = "cp936",
                         auto_close_when_success = false,
